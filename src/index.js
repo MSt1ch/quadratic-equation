91b5adcb4b7ebe1fe14e1719f48e08dd6674f757
St1ch <st1ch@mail.ru>
2018-02-22 22:59:59 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+	'-20 * x^2 - 10 * x - 13'
+  var arr = equation.split(' ');
+  var a, b, c, d, x1, x2, solutions = [];
+  a = arr[0];
+  b = arr[3] + arr[4];
+  c = arr[7] + arr[8];
+  d = b*b - 4 * a * c;
+
+  x1 = Math.round((- b) + Math.sqrt(d)) / (2 * a)
+  x2 = Math.round((- b) - Math.sqrt(d)) / (2 * a)
+  solutions.push(x1, x2);
+  solutions.sort( (a,b) => a - b );
+  return solutions
 }
